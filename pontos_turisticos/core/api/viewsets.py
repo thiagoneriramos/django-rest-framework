4c7f742f991e640e9739a9d03e5c5b0e2148aca7
thiagoneriramos <thiagoneriramos79@gmail.com>
2018-11-14 07:54:18 -0300

    Sobrescrevendo a action de GET

--- pontos_turisticos/core/api/viewsets.py
+++ pontos_turisticos/core/api/viewsets.py
@@ -1,3 +1,4 @@
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from core.models import PontoTuristico
 from .serializers import PontoTuristicoSerializer
@@ -9,3 +10,8 @@ class PontoTuristicoViewSet(ModelViewSet):
 
     def get_queryset(self):
         return PontoTuristico.objects.filter(aprovado=True)
+
+
+    def list(self, request, *args, **kwargs):
+        return Response({'teste': 123})
+
