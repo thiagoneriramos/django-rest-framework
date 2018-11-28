ced849edbf1f1e2baa8e408234e24ef2fce5a772
thiagoneriramos <thiagoneriramos79@gmail.com>
2018-11-28 09:13:45 -0300

    Implementando NestedRelationships em atracoes e enderecos

--- pontos_turisticos/core/api/serializers.py
+++ pontos_turisticos/core/api/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework.serializers import ModelSerializer
 from core.models import PontoTuristico
+from atracoes.api.serializers import AtracaoSerializer
+from enderecos.api.serializers import EnderecoSerializer
 
 class PontoTuristicoSerializer(ModelSerializer):
+    atracoes = AtracaoSerializer(many=True)
+    endereco = EnderecoSerializer()
+
     class Meta:
         model = PontoTuristico
-        fields = ('id', 'nome', 'descricao', 'aprovado', 'foto')
+        fields = ('id', 'nome', 'descricao', 'aprovado', 'foto',
+                  'atracoes', 'comentarios', 'avaliacoes',  'endereco'
+
+        )
